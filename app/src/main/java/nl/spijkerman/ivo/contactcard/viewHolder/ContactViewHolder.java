18613f2870dcbc96e3eae4ec6afafee4fbb6c874
Ivo Spijkerman <ip.spijkerman@avans.nl>
2018-11-21 16:46:26 +0100

    added some untraked files

---
+++ app/src/main/java/nl/spijkerman/ivo/contactcard/viewHolder/ContactViewHolder.java
@@ -0,0 +1,11 @@
+package nl.spijkerman.ivo.contactcard.viewHolder;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ContactViewHolder {
+    public ImageView image;
+    public TextView name;
+    public TextView email;
+
+}
